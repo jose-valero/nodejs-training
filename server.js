0cced8c5002859aa1514d891510b1e13c45f1c28
jose valero <josemiguelvaleroreyes@gmail.com>
2020-02-19 18:44:11 -0300

    backend de proyecto para chat

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
  
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const socket = require('./socket');
 const db = require('mongoose');
 const router = require('./network/routes')
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
